d87a18691b3bba4f57ca637830a94c3d5ee26d44
Hafid Dian Nurfaujan Ahat <ambonxnobody.15072001@gmail.com>
2024-11-05 07:42:06 +0700

    Save Progress

--- entity/global/model.go
+++ entity/global/model.go
@@ -10,10 +10,13 @@ type Model struct {
 }
 
 func (m *Model) Created(userID string) {
+	// set metadata created_by, created_at, updated_by, updated_at
 }
 
 func (m *Model) Updated(userID string) {
+	// set metadata updated_by, updated_at
 }
 
 func (m *Model) SoftDelete(userID string) {
+	// set metadata deleted_by, deleted_at
 }
--- entity/tag.go
+++ entity/tag.go
@@ -1,1 +1,9 @@
 package entity
+
+import "github.com/hafiddna/auth-starterkit-be/entity/global"
+
+type Tag struct {
+	global.Model
+	Name   string   `gorm:"type:varchar(255);unique" json:"name"`
+	Assets []*Asset `gorm:"many2many:asset_tag" json:"assets"`
+}
