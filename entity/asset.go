331ee8b460d35280b54da6592678065686155390
Hafid Dian Nurfaujan Ahat <ambonxnobody.15072001@gmail.com>
2024-11-05 07:45:28 +0700

    Save Progress

--- entity/asset.go
+++ entity/asset.go
@@ -15,7 +15,7 @@ type Asset struct {
 	Path         string         `gorm:"type:varchar(255)" json:"path"`
 	Size         int64          `gorm:"type:bigint;default:0" json:"size"`
 	Type         string         `gorm:"type:varchar(255);default:'file';comment:'document, spreadsheet, presentation, form, image, pdf, video, shortcut, site, audio, drawing, archive, file'" json:"type"`
-	Access       string         `gorm:"type:enum('public', 'private');default:'private'" json:"access"`
+	Access       string         `gorm:"type:varchar(255);default:'viewer';comment:'viewer, editor, owner'" json:"access"`
 	BucketName   string         `gorm:"type:varchar(255)" json:"bucket_name"`
 	IsPublic     bool           `gorm:"type:boolean;default:false" json:"is_public"`
 	FileMetadata datatypes.JSON `gorm:"type:jsonb;null" json:"file_metadata"`
