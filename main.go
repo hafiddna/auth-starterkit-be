f365a42f72626a9a86de03a82c765a88c1d076ac
Hafid DNA <hafiddna@gmail.com>
2025-01-12 08:39:58 +0700

    Save Progress

--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 import (
 	validator2 "github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/etag"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 	"github.com/hafiddna/auth-starterkit-be/config"
 	"github.com/hafiddna/auth-starterkit-be/controller"
 	"github.com/hafiddna/auth-starterkit-be/database"
@@ -10,7 +16,9 @@ import (
 	"github.com/hafiddna/auth-starterkit-be/middleware"
 	"github.com/hafiddna/auth-starterkit-be/repository"
 	"github.com/hafiddna/auth-starterkit-be/service"
+	"github.com/hafiddna/auth-starterkit-be/tool"
 	"github.com/hafiddna/auth-starterkit-be/util"
+	"log"
 	"time"
 )
 
@@ -48,19 +56,19 @@ func main() {
 	}
 
 	// Minio
-	// TODO: Enable Minio on Creating File Upload Feature
-	//minio, err := tool.ConnectToMinio()
-	//if err != nil {
-	//	panic(err)
-	//}
+	// TODO: Use Minio for file storage
+	_, err = tool.ConnectToMinio()
+	if err != nil {
+		panic(err)
+	}
 
 	// Fiber
 	app := fiber.New(fiber.Config{
 		//Prefork:       true,
-		//CaseSensitive: true,
-		//StrictRouting: true,
-		AppName:      config.Config.App.Name,
-		ServerHeader: config.Config.App.ServerName,
+		CaseSensitive: true,
+		StrictRouting: true,
+		AppName:       config.Config.App.Name,
+		ServerHeader:  config.Config.App.ServerName,
 		ErrorHandler: func(c *fiber.Ctx, err error) error {
 			return helper.SendResponse(helper.ResponseStruct{
 				Ctx:        c,
@@ -75,11 +83,15 @@ func main() {
 	validator := validator2.New()
 
 	// Global Middleware
-	app.Use(middleware.CORSMiddleware())
-	app.Use(middleware.LoggerMiddleware)
-	//app.Use(cors.New())
-	//app.Use(compress.New())
-	//app.Use(etag.New())
+	// TODO: Recheck this middleware
+	app.Use(cors.New(cors.Config{
+		AllowOriginsFunc: nil,
+		AllowOrigins:     config.Config.App.Server.Cors,
+		AllowMethods:     "GET, POST, PATCH, PUT, DELETE",
+		AllowHeaders:     "Accept, Content-Type, Content-Length, Accept-Encoding, Accept-Language, X-CSRF-Token, Authorization, X-Requested-With, User-Agent, Connection, Host",
+	}))
+	app.Use(compress.New())
+	app.Use(etag.New())
 	//app.Use(favicon.New())
 	//app.Use(limiter.New(limiter.Config{
 	//	Max: 100,
@@ -90,9 +102,32 @@ func main() {
 	//		})
 	//	},
 	//}))
-	//app.Use(logger.New())
-	//app.Use(recover.New())
-	//app.Use(requestid.New())
+	app.Use(recover.New())
+	app.Use(requestid.New())
+	//app.Use(middleware.LoggerMiddleware)
+	//file, err := os.OpenFile("./123.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	//if err != nil {
+	//	log.Fatalf("error opening file: %v", err)
+	//}
+	//defer file.Close()
+	app.Use(logger.New(logger.Config{
+		Format:     "Time: ${time} | Status: ${status} | PID: ${pid} | RequestID: ${locals:requestid} | Latency: ${latency} | IP: [${ip}]:${port} | Method: ${method} | Path: ${path} | Error: ${error}\n",
+		TimeFormat: time.RFC3339Nano,
+		TimeZone:   "UTC",
+		Done: func(c *fiber.Ctx, logString []byte) {
+			log.Println(string(logString))
+			//if c.Response().StatusCode() != fiber.StatusOK {
+			//reporter.SendToSlack(logString)
+			//}
+		},
+		//CustomTags: map[string]logger.LogFunc{
+		//	"custom_tag": func(output logger.Buffer, c *fiber.Ctx, data *logger.Data, extraParam string) (int, error) {
+		//		return output.WriteString("it is a custom tag")
+		//	},
+		//},
+		//DisableColors: true,
+		//Output: file,
+	}))
 
 	// Repository
 	roleUserRepository := repository.NewRoleUserRepository(postgreSQL)
