a67b773438881a98cd8b5586010ff777bc83e4e8
Hafid DNA <hafiddna@gmail.com>
2025-03-08 17:06:38 +0700

    Save Progress

--- repository/user.go
+++ repository/user.go
@@ -27,17 +27,18 @@ func NewUserRepository(db *gorm.DB, minio *minio.Client) UserRepository {
 
 func (r *userRepository) FindByEmailPhoneOrUsername(credential string) (user model.User, err error) {
 	query := r.db.Where("email = ?", credential).Or("phone = ?", credential).Or("username = ?", credential).
-		Preload("Roles", "team_id IS NULL").Preload("Roles.Permissions")
-	if config.Config.App.AuthConfig.IsTeamEnabled {
-		query = query.Preload("Teams", func(db *gorm.DB) *gorm.DB {
-			return db.Joins("JOIN team_user ON team_user.team_id = teams.id").
-				Where("team_user.is_active = ?", true)
-		}).Preload("Teams.Roles").Preload("Teams.Roles.Permissions").
-			Preload("MembersOf", func(db *gorm.DB) *gorm.DB {
-				return db.Joins("JOIN team_user ON team_user.team_id = teams.id").
-					Where("team_user.is_active = ?", true)
-			}).Preload("MembersOf.Roles").Preload("MembersOf.Roles.Permissions")
-	}
+		Preload("Roles").Preload("Roles.Permissions")
+	//Preload("Roles", "team_id IS NULL").Preload("Roles.Permissions")
+	//if config.Config.App.AuthConfig.IsTeamEnabled {
+	//	query = query.Preload("Teams", func(db *gorm.DB) *gorm.DB {
+	//		return db.Joins("JOIN team_user ON team_user.team_id = teams.id").
+	//			Where("team_user.is_active = ?", true)
+	//	}).Preload("Teams.Roles").Preload("Teams.Roles.Permissions").
+	//		Preload("MembersOf", func(db *gorm.DB) *gorm.DB {
+	//			return db.Joins("JOIN team_user ON team_user.team_id = teams.id").
+	//				Where("team_user.is_active = ?", true)
+	//		}).Preload("MembersOf.Roles").Preload("MembersOf.Roles.Permissions")
+	//}
 	err = query.First(&user).Error
 	if err != nil {
 		return user, err
@@ -62,17 +63,18 @@ func (r *userRepository) FindOneById(id string) (user model.User, err error) {
 }
 
 func (r *userRepository) FindByIDWithTokenData(id string) (user model.User, err error) {
-	query := r.db.Where("id = ?", id).Preload("Roles", "team_id IS NULL").Preload("Roles.Permissions")
-	if config.Config.App.AuthConfig.IsTeamEnabled {
-		query = query.Preload("Teams", func(db *gorm.DB) *gorm.DB {
-			return db.Joins("JOIN team_user ON team_user.team_id = teams.id").
-				Where("team_user.is_active = ?", true)
-		}).Preload("Teams.Roles").Preload("Teams.Roles.Permissions").
-			Preload("MembersOf", func(db *gorm.DB) *gorm.DB {
-				return db.Joins("JOIN team_user ON team_user.team_id = teams.id").
-					Where("team_user.is_active = ?", true)
-			}).Preload("MembersOf.Roles").Preload("MembersOf.Roles.Permissions")
-	}
+	query := r.db.Where("id = ?", id).Preload("Roles").Preload("Roles.Permissions")
+	//query := r.db.Where("id = ?", id).Preload("Roles", "team_id IS NULL").Preload("Roles.Permissions")
+	//if config.Config.App.AuthConfig.IsTeamEnabled {
+	//	query = query.Preload("Teams", func(db *gorm.DB) *gorm.DB {
+	//		return db.Joins("JOIN team_user ON team_user.team_id = teams.id").
+	//			Where("team_user.is_active = ?", true)
+	//	}).Preload("Teams.Roles").Preload("Teams.Roles.Permissions").
+	//		Preload("MembersOf", func(db *gorm.DB) *gorm.DB {
+	//			return db.Joins("JOIN team_user ON team_user.team_id = teams.id").
+	//				Where("team_user.is_active = ?", true)
+	//		}).Preload("MembersOf.Roles").Preload("MembersOf.Roles.Permissions")
+	//}
 
 	err = query.First(&user).Error
 	if err != nil {
